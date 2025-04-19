d75c33f2f19c92db8393d7559b39de56b5950eb6
donatello <marilynbraojos@gmail.com>
2025-04-19 16:57:25 -0400

    camera not working

--- ironman_navigate_to_goal/go_to_goal.py
+++ ironman_navigate_to_goal/go_to_goal.py
@@ -8,9 +8,9 @@ from geometry_msgs.msg import Twist, Vector3Stamped
 import math
 import numpy as np
 
-WAYPOINTS_FILE = "wayPoints.txt" # file containing waypoints
-WAYPOINT_TOLERANCES = [0.05, 0.05, 0.05, 0.05] # waypoint tolerances
-STOP_DURATIONS = [10, 10, 2, 10] # stopping time at each waypoint
+# WAYPOINTS_FILE = "wayPoints.txt" # file containing waypoints
+# WAYPOINT_TOLERANCES = [0.05, 0.05, 0.05, 0.05] # waypoint tolerances
+# STOP_DURATIONS = [10, 10, 2, 10] # stopping time at each waypoint
 
 class GoToGoal(Node):
     def __init__(self):
@@ -20,9 +20,9 @@ class GoToGoal(Node):
         self.odom_sub = self.create_subscription(Odometry, '/odom', self.odom_callback, 10) # subscribe to odometry readings
         self.obs_sub = self.create_subscription(Vector3Stamped, '/detected_distance', self.lidar_callback, 10) # subscribe to object location
 
-        self.waypoints = self.read_waypoints() # load waypoints 
+        # self.waypoints = self.read_waypoints() # load waypoints 
 
-        self.goal_index = 0 # index of current waypoint
+        # self.goal_index = 0 # index of current waypoint
         self.yaw = 0.0 # initialize orientation
         self.init_yaw = 0.0 # orientation from the start
         self.init_x = 0.0 # initialize starting position
@@ -33,22 +33,22 @@ class GoToGoal(Node):
         self.odom_offset = None # flag to trigger offset at start
         self.init_pos = None # initialize starting position
         self.rotation_matrix = None # initialize rotate global frame to initial orientation 
-        self.reached_time = None # initialize time goal was reached
+        # self.reached_time = None # initialize time goal was reached
         self.obstacle_distance = None # initialize distane of obstacle
         self.avoid_start_time = None # initialize time that avoidance began
 
         self.avoiding_obstacle = False # initialize whether avoidance is ongoing
         
-        self.start_time = self.get_clock().now().seconds_nanoseconds()[0] # global timer
+        # self.start_time = self.get_clock().now().seconds_nanoseconds()[0] # global timer
         self.timer = self.create_timer(0.1, self.controller_loop) # timer to call the control loop 
 
-    def read_waypoints(self): # read waypoints from text file
-        waypoints = [] # initialize list
-        with open(WAYPOINTS_FILE, 'r') as f: # read file 
-            for line in f: # for every line in the file
-                x, y = map(float, line.strip().split()) # x and y are separated by a space
-                waypoints.append((x, y)) # add the read x and y to the list
-        return waypoints # return the waypoints when this fcn is called
+    # def read_waypoints(self): # read waypoints from text file
+    #     waypoints = [] # initialize list
+    #     with open(WAYPOINTS_FILE, 'r') as f: # read file 
+    #         for line in f: # for every line in the file
+    #             x, y = map(float, line.strip().split()) # x and y are separated by a space
+    #             waypoints.append((x, y)) # add the read x and y to the list
+    #     return waypoints # return the waypoints when this fcn is called
 
     def odom_callback(self, msg): # setting up initial odometry offset for frame transforms 
         position = msg.pose.pose.position # get current position 
@@ -86,9 +86,9 @@ class GoToGoal(Node):
         now = self.get_clock().now().nanoseconds / 1e9 # obtaining current time 
 
         # Timeout check
-        if self.goal_index >= len(self.waypoints) or (now - self.start_time > 150): # if the goal index exceeds the len of the waypoints or if 2 min 30 s reached, terminate
-            self.cmd_pub.publish(Twist()) # stop robot motors
-            return
+        # if self.goal_index >= len(self.waypoints) or (now - self.start_time > 150): # if the goal index exceeds the len of the waypoints or if 2 min 30 s reached, terminate
+            # self.cmd_pub.publish(Twist()) # stop robot motors
+            # return
 
         # Obstacle avoidance logic
         if self.avoiding_obstacle: # if obstacle avoidance is initiated
@@ -131,31 +131,37 @@ class GoToGoal(Node):
                     self.avoid_start_time = None # reset timer 
                     self.cmd_pub.publish(Twist()) # stop robot 
                     return
+        # else: 
+        #     cmd = Twist()
+        #     cmd.linear.x = 0.15
+        #     self.cmd_pub.publish(cmd)
+
 
         # Waypoint navigation
-        goal = self.waypoints[self.goal_index] # get current waypoint
-        dx = goal[0] - self.current_position[0] # difference in x from current to goal 
-        dy = goal[1] - self.current_position[1] # difference in y from current to goal 
-        distance = math.sqrt(dx**2 + dy**2) # evaluate distance to waypoint
+        # goal = self.waypoints[self.goal_index] # get current waypoint
+        # dx = goal[0] - self.current_position[0] # difference in x from current to goal 
+        # dy = goal[1] - self.current_position[1] # difference in y from current to goal 
+        # distance = math.sqrt(dx**2 + dy**2) # evaluate distance to waypoint
 
-        if distance < WAYPOINT_TOLERANCES[self.goal_index]: # if the distance is not within tolerance
-            if self.reached_time is None: 
-                self.reached_time = now # starting the waiting at each position timer 
-            elif now - self.reached_time >= STOP_DURATIONS[self.goal_index]: # move on to next index after timer is done
-                self.goal_index += 1
-                self.reached_time = None # reset timer 
-            self.cmd_pub.publish(Twist())
-            return
+        # if distance < WAYPOINT_TOLERANCES[self.goal_index]: # if the distance is not within tolerance
+            # if self.reached_time is None: 
+                # self.reached_time = now # starting the waiting at each position timer 
+            # elif now - self.reached_time >= STOP_DURATIONS[self.goal_index]: # move on to next index after timer is done
+                # self.goal_index += 1
+                # self.reached_time = None # reset timer 
+            # self.cmd_pub.publish(Twist())
+            # return
 
-        angle_to_goal = math.atan2(dy, dx) # angle from robot to goal 
-        angle_diff = math.atan2(math.sin(angle_to_goal - self.yaw), math.cos(angle_to_goal - self.yaw)) # get shortest rotation direction to the goal
+        # angle_to_goal = math.atan2(dy, dx) # angle from robot to goal 
+        # angle_diff = math.atan2(math.sin(angle_to_goal - self.yaw), math.cos(angle_to_goal - self.yaw)) # get shortest rotation direction to the goal
 
         cmd = Twist()
-        if abs(angle_diff) > 0.1:
-            cmd.angular.z = 0.5 * angle_diff
-        else:
-            cmd.linear.x = 0.15
-            cmd.angular.z = 0.3 * angle_diff
+        cmd.linear.x = 0.15
+        # if abs(angle_diff) > 0.1:
+            # cmd.angular.z = 0.5 * angle_diff
+        # else:
+            # cmd.linear.x = 0.15
+            # cmd.angular.z = 0.3 * angle_diff
 
         self.cmd_pub.publish(cmd)
 
